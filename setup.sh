aaae539f31e403964902aa1a008d73b620b5f6a2
Nick Fox-Gieg <nick@fox-gieg.com>
2020-12-19 15:08:54 -0500

    shell script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
-cd Model
-wget https://fox-gieg.com/patches/github/n1ckfg/ArtLine/ArtLine_500.pkl
-wget https://fox-gieg.com/patches/github/n1ckfg/ArtLine/ArtLine_650.pkl
+pip3 install cython numpy==1.17.2 
+pip3 install -r ./Model/requirements.txt
+
+#cd Model
+#wget https://fox-gieg.com/patches/github/n1ckfg/ArtLine/ArtLine_500.pkl
+#wget https://fox-gieg.com/patches/github/n1ckfg/ArtLine/ArtLine_650.pkl
