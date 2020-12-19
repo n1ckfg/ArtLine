c04e8becb27a70c2f26fbb7a7e8aed1feb34f63a
Nick Fox-Gieg <nick@fox-gieg.com>
2020-12-19 15:24:18 -0500

    shell script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 pip3 install cython numpy==1.17.2 
 pip3 install -r ./Model/requirements.txt
+pip3 install -r colab_requirements.txt
 
-#cd Model
-#wget https://fox-gieg.com/patches/github/n1ckfg/ArtLine/ArtLine_500.pkl
-#wget https://fox-gieg.com/patches/github/n1ckfg/ArtLine/ArtLine_650.pkl
+cd Model
+wget https://fox-gieg.com/patches/github/n1ckfg/ArtLine/ArtLine_500.pkl
+wget https://fox-gieg.com/patches/github/n1ckfg/ArtLine/ArtLine_650.pkl
